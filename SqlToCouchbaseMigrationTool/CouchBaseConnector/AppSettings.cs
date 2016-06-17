bf895495a355b70b84242ba1c88f247963ae7e93
tavisca-spatil <spatil@tavisca.com>
2016-06-17 17:33:03 +0530

    Added CouchbaseConnector

---
+++ SqlToCouchbaseMigrationTool/CouchBaseConnector/AppSettings.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CouchBaseConnector
+{
+    internal static class AppSettings
+    {
+        public static string DefaultBucket
+        {
+            get { return "default"; }
+        }
+
+        public static string BeerBucket
+        {
+            get { return "mTravelCredit"; }
+        }
+
+    }
+}
---
+++ SqlToCouchbaseMigrationTool/CouchBaseConnector/Connector.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Couchbase;
+using Couchbase.IO;
+using Couchbase.N1QL;
+using Couchbase.Views;
+
+
+namespace CouchBaseConnector
+{
+    public class Connector
+    {
+        public bool Save<T>(string key, T obj, TimeSpan timeSpan)
+        {
+            using (var cluster = new Cluster())
+            {
+                using (var bucket = cluster.OpenBucket(AppSettings.DefaultBucket))
+                {
+                    IOperationResult res = bucket.Upsert(key, obj, timeSpan);
+                    if (res.Status == ResponseStatus.Success)
+                        return true;
+                }
+            }
+            return false;
+        }
+
+        public T Get<T>(string key)
+        {
+            using (var cluster = new Cluster())
+            {
+                using (var bucket = cluster.OpenBucket(AppSettings.DefaultBucket))
+                {
+                    IOperationResult<T> res = bucket.Get<T>(key);
+                    if (res.Status == ResponseStatus.Success)
+                    {
+                        return res.Value;
+                    }
+                    throw new Exception("Data not found");
+                }
+            }
+        }
+
+        public List<dynamic> Query(IQueryRequest query)
+        {
+            using (var cluster = new Cluster())
+            {
+                using (var bucket = cluster.OpenBucket(AppSettings.DefaultBucket))
+                {
+                    var res = bucket.Query<dynamic>(query);
+
+                    var resultSet = new List<dynamic>();
+
+                    foreach (var row in res.Rows)
+                    {
+                        resultSet.Add(row);
+                    }
+                    return resultSet;
+                }
+            }
+        }
+
+        public List<T> Query<T>(string view, string queryName)
+        {
+            using (var cluster = new Cluster())
+            {
+                using (var bucket = cluster.OpenBucket(AppSettings.DefaultBucket))
+                {
+                    var query = bucket.CreateQuery(view, queryName, true).Limit(5);
+                    var resultSet = new List<T>();
+                    var result = bucket.Query<T>(query);
+                    foreach (ViewRow<T> row in result.Rows)
+                    {
+                        resultSet.Add(row.Value);
+                    }
+                    return resultSet;
+                }
+            }
+        }
+    }
+}
