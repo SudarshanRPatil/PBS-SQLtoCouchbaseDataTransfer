dabc5fe487bbfd5ac89c85395bf97a3fcab734c0
tavisca-anair <anair@tavisca.com>
2016-06-20 09:22:21 +0530

    added translator to convert sql data to cb transaction detail

--- SqlToCouchbaseMigrationTool/DataConsolidator/Translator.cs
+++ SqlToCouchbaseMigrationTool/DataConsolidator/Translator.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DataAccess;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,74 @@ using System.Threading.Tasks;
 
 namespace DataConsolidator
 {
-    class Translator
+    public static class Translator
     {
+        internal static List<TransactionDetails> ToCBTransactionDetail(List<spGetTranscationDetailsResult> transactions)
+        {
+            if (transactions != null && transactions.Count > 0)
+            {
+                List<TransactionDetails> cbTransactiondetails = new List<TransactionDetails>();
+                foreach (var transaction in transactions)
+                {
+                    var transactionDetail = new TransactionDetails
+                    {
+                        DocumentId = new Guid().ToString(),
+                        TransactionId = transaction.TransactionId.ToString(),
+                        ReferenceTransactionId = transaction.ReferenceTransactionId.ToString(),
+                        TransactionType = transaction.TransactionType.ToCBTransactionType(),
+                        Currency = transaction.Currency,
+                        Amount = transaction.Amount,
+                        ConversionFactor = transaction.ConversionFactor,
+                        USDEquivalentAmount = transaction.UsdEquivalentAmount,
+                        SoftCashProgram = transaction.SoftcashProgram.ToCBSoftCashProgram(),
+                        Timestamp = transaction.Timestamp,
+                        Reason = transaction.Reason,
+                        Comment = transaction.Comment,
+                        Validity = transaction.Validity,
+                        Availability = transaction.Availability.ToCBAvailabilityType(),
+                        TransactionStatus = transaction.TransactionStatus.ToCBTransactionStatus(),
+                        //ClosingAmount = transaction.ClosingAmount,
+                        //Owner  
+                        Source = new Model.Source
+                        {
+                            Application = transaction.Application,
+                        }
+                        RequesterName = transaction.RequesterUserName,
+                        //RedemptionDetails
+                    };
+                }
+                return cbTransactiondetails;
+            }
+            return new List<TransactionDetails>();
+            
+        }
+
+        internal static TransactionType ToCBTransactionType(this string transactionType)
+        {
+            TransactionType cbTransactionType;
+            Enum.TryParse(transactionType, out cbTransactionType);
+            return cbTransactionType;
+        }
+
+        internal static CategoryType ToCBSoftCashProgram(this string softcashProgram)
+        {
+            CategoryType cbsoftcashProgram;
+            Enum.TryParse(softcashProgram, out cbsoftcashProgram);
+            return cbsoftcashProgram;
+        }
+
+        internal static AvailibilityType ToCBAvailabilityType(this string availability)
+        {
+            AvailibilityType cbAvailabilityType;
+            Enum.TryParse(softcashProgram, out cbAvailabilityType);
+            return cbAvailabilityType;
+        }
+
+        internal static TransactionStatusType ToCBTransactionStatus(this string transactionStatus)
+        {
+            TransactionStatusType cbTransactionStatus;
+            Enum.TryParse(transactionStatus, out cbTransactionStatus);
+            return cbTransactionStatus;
+        }
     }
 }
