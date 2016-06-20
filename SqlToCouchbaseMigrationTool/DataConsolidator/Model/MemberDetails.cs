c277e295a04a4c270cd2d244ed1056dfd8278de0
Sheetal Mohite <smohite@tavisca.com>
2016-06-20 10:54:30 +0530

    get account details from vexiere

---
+++ SqlToCouchbaseMigrationTool/DataConsolidator/Model/MemberDetails.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataConsolidator.Model
+{
+    public class MemberDetails
+    {
+        public string MemberName { get; set; }
+        public long Code { get; set; }
+        public string MemberShips { get; set; }
+        public string MemberShipStatuses { get; set; }
+        public string MemberCountry { get; set; }
+    }
+}
